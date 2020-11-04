233125467aed505e29b6b8d0aa08f6ccc08638f6
Chase Grainger <chase.grainger2134@gmail.com>
2020-11-04 13:01:42 -0500

    integrated junit

--- src/edu/indwes/cis330/arrays/Main.java
+++ src/edu/indwes/cis330/arrays/Main.java
@@ -2,13 +2,17 @@ package edu.indwes.cis330.arrays;
 
 public class Main {
 	public static void main(String[] args) {
+		
+		// Create items
 		ArraysAPI arrAPI = new ArraysAPI();
 		int arr[] = new int[]{1,2,3,4,5};
+		
+		// Run add item method
 		arrAPI.addItem(2, arr, 1);
 		
+		// Run remove item method
 		arrAPI.removeItem(2, arr);
-		for (int i = 0; i < arr.length;i++)
-        {
+		for (int i = 0; i < arr.length;i++) {
             System.out.println(arr[i]);
         }
 	}
