28331b8c4f4627a1f4c44cb7e298e658a75cea9c
mauleenn <mauleen.ndlovu@outlook.com>
2020-11-05 22:39:02 -0600

    Added PPC file and CFG
    Added the PPC.java file and I also created the CFG for addItem.

---
+++ src/edu/indwes/cis330/arrays/PPC.java
@@ -0,0 +1,24 @@
+package edu.indwes.cis330.arrays;
+import org.junit.Test;
+
+
+//Create first the control flow graph (CFG)
+
+
+public class PPC {
+
+    @Test
+	public void testAddItem() {
+        // Insert Item
+		
+    }
+    
+
+
+
+    @Test
+	public void testRemoveItem() {
+		// Remove Item 
+	}
+    
+}
